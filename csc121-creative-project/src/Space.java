e3ef06e036bede7e8958d33f7820b24bba9615a5
N8Wr1ght <Nate.Wright@vikings.berry.edu>
2024-09-23 22:01:00 -0400

    added cool stuff

--- csc121-creative-project/src/Space.java
+++ csc121-creative-project/src/Space.java
@@ -2,78 +2,129 @@ import processing.core.PApplet;
 import processing.event.KeyEvent;
 
 public class Space implements IWorld {
-    Posn shipPos;
-    Posn alienPos;
-    Posn asteroidPos;
-    float shipSize = 30;
-    float objectSize = 20;
-    int score = 0;
-    boolean gameOver = false;
+	Posn shipPos;
+	Posn alienPos;
+	Posn asteroidPos;
+	float shipSize = 30;
+	float objectSize = 50;
+	int score = 0;
+	boolean gameOver = false;
+	boolean moveUp = false;
+	boolean moveDown = false;
+	boolean moveLeft = false;
+	boolean moveRight = false;
 
-    public Space() {
-        shipPos = new Posn(200, 200);
-        alienPos = new Posn((int) (Math.random() * 380), (int) (Math.random() * 380));
-        asteroidPos = new Posn((int) (Math.random() * 380), (int) (Math.random() * 380));
-    }
+	public Space() {
+		shipPos = new Posn(200, 200);
+		alienPos = new Posn((int) (Math.random() * 380), -25);
+		asteroidPos = new Posn((int) (Math.random() * 380), -10);
+	}
 
-    @Override
-    public IWorld update() {
-        if (shipPos.distanceTo(alienPos) < shipSize / 2 + objectSize / 2) {
-            score += 1;
-            alienPos = new Posn((int) (Math.random() * 380), (int) (Math.random() * 380));
-        }
-        
-        if (shipPos.distanceTo(asteroidPos) < shipSize / 2 + objectSize / 2) {
-            gameOver = true;
-        }
+	@Override
+	public IWorld update() {
+		if (shipPos.x - shipSize / 4 <= alienPos.x + objectSize && 
+				shipPos.x + shipSize / 4 >= alienPos.x && 
+				shipPos.y - shipSize / 4 >= alienPos.y &&
+				shipPos.y + shipSize / 4 <= alienPos.y + objectSize) {
+			score += 1;
+			alienPos = new Posn((int) (Math.random() * 380), -25);
+		}
 
-        return this;
-    }
+		if (shipPos.x - shipSize / 4 <= asteroidPos.x + objectSize && 
+				shipPos.x + shipSize / 4 >= asteroidPos.x && 
+				shipPos.y - shipSize / 4 >= asteroidPos.y &&
+				shipPos.y + shipSize / 4 <= asteroidPos.y + objectSize) {
+			gameOver = true;
+		}
+		if (asteroidPos.y >= 400) {
+			asteroidPos = new Posn((int) (Math.random() * 380), -25);
+		}
+		asteroidPos.y = asteroidPos.y + 5;
 
-    @Override
-    public PApplet draw(PApplet p) {
-        p.background(255);
-        if (!gameOver) {
-            // Draw spaceship
-            p.fill(0, 0, 255);
-            p.ellipse(shipPos.getX(), shipPos.getY(), shipSize, shipSize);
+		alienPos.y = alienPos.y + 1;
 
-            // Draw alien
-            p.fill(0, 255, 0);
-            p.rect(alienPos.getX(), alienPos.getY(), objectSize, objectSize);
+		if (moveUp) {
+			shipPos = shipPos.translate(new Posn(0, -5));
+		}
+		if (moveDown) {
+			shipPos = shipPos.translate(new Posn(0, 5));
+		}
+		if (moveLeft) {
+			shipPos = shipPos.translate(new Posn(-5, 0));
+		}
+		if (moveRight) {
+			shipPos = shipPos.translate(new Posn(5, 0));
+		}
 
-            // Draw asteroid
-            p.fill(255, 0, 0);
-            p.rect(asteroidPos.getX(), asteroidPos.getY(), objectSize, objectSize);
 
-            // Draw score
-            p.fill(0);
-            p.textSize(16);
-            p.text("Score: " + score, 10, 20);
-        } else {
-            p.fill(0);
-            p.textSize(32);
-            p.text("Game Over", 120, 200);
-        }
-        return p;
-    }
+		return this;
+	}
 
-    @Override
-    public IWorld keyPressed(KeyEvent kev) {
-        switch (kev.getKeyCode()) {
-            case PApplet.UP:
-                shipPos = shipPos.translate(new Posn(0, -10));
-                break;
-            case PApplet.DOWN:
-                shipPos = shipPos.translate(new Posn(0, 10));
-                break;
-            case PApplet.LEFT:
-                shipPos = shipPos.translate(new Posn(-10, 0));
-                break;
-            case PApplet.RIGHT:
-                shipPos = shipPos.translate(new Posn(10, 0));
-                break;
-        }
-        return this;
-    }
+	@Override
+	public PApplet draw(PApplet p) {
+		p.background(0);
+		if (!gameOver) {
+			// Draw spaceship
+			p.fill(0, 0, 255);
+			p.ellipse(shipPos.getX(), shipPos.getY(), shipSize, shipSize);
+
+			// Draw alien
+			p.fill(0, 255, 0);
+			p.rect(alienPos.getX(), alienPos.getY(), objectSize, objectSize);
+
+			// Draw asteroid
+			p.fill(255, 0, 0);
+			p.rect(asteroidPos.getX(), asteroidPos.getY(), objectSize, objectSize);
+
+			// Draw score
+			p.fill(255);
+			p.textSize(16);
+			p.text("Score: " + score, 10, 20);
+		} else {
+			p.fill(255);
+			p.textSize(32);
+			p.text("Game Over", 120, 200);
+			p.text("Score: " + score, 120, 250);
+		}
+		return p;
+	}
+
+	@Override
+	public IWorld keyPressed(KeyEvent kev) {
+		switch (kev.getKeyCode()) {
+		case PApplet.UP:
+			moveUp = true;
+			break;
+		case PApplet.DOWN:
+			moveDown = true;
+			break;
+		case PApplet.LEFT:
+			moveLeft = true;
+			break;
+		case PApplet.RIGHT:
+			moveRight = true;
+			break;
+		}
+		return this;
+	}
+
+	/** produce an updated state of this world after a key release event */
+	public IWorld keyReleased(KeyEvent kev) { 
+		switch (kev.getKeyCode()) {
+		case PApplet.UP:
+			moveUp = false;
+			break;
+		case PApplet.DOWN:
+			moveDown = false;
+			break;
+		case PApplet.LEFT:
+			moveLeft = false;
+			break;
+		case PApplet.RIGHT:
+			moveRight = false;
+			break;
+
+		}
+		return this; 
+	}
 }
